04df577b0b0de505b0a81d743f62de3373ef6fd6
24DP2Mcere <A240480MC@rvt.lv>
2025-09-09 13:37:16 +0300

    Messaage uzd un Persona uzd

---
+++ src/main/java/rvt/Person.java
@@ -0,0 +1,12 @@
+package rvt;
+
+public class Person {
+    
+    private String name; 
+    private int age ; 
+    private int weight ;
+    private int height ;
+    private String address ;
+
+
+}
