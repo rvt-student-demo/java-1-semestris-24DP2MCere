fd3e8cef9a8ab67a3e590fc615ffdb1daad448ca
24DP2Mcere <A240480MC@rvt.lv>
2025-09-10 13:07:50 +0300

    VariousVariables  uzd (gatavs)

---
+++ src/main/java/rvt/VariousVariables.java
@@ -0,0 +1,19 @@
+package rvt;
+
+public class VariousVariables {
+
+    public static void main (String[] args){
+
+        int chicken = 9000 ;
+        double bacon_kg = 0.1 ;
+        Boolean fort = true ;
+        String tractor_name = "Zetor" ;
+
+        System.out.println("Chicken:" + "\n" + chicken);
+        System.out.println("Bacon(kg)" + "\n" + bacon_kg);
+        System.out.println("Tractor" + "\n" + tractor_name);
+        System.out.println("And finally, a summary" + "\n" + chicken  + "\n" + bacon_kg + "\n" + tractor_name);
+
+    }
+    
+}
