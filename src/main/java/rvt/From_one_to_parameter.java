186b05b725f04b40c6a5476449bd3a376e62eb30
24DP2Mcere <A240480MC@rvt.lv>
2025-10-07 12:33:20 +0300

    From parameter to one and from one to parameter

---
+++ src/main/java/rvt/From_one_to_parameter.java
@@ -0,0 +1,17 @@
+package rvt;
+
+public class From_one_to_parameter {
+    
+public static void main(String[] args) {
+    printUntilNumber(5);
+}
+
+
+    public static void printUntilNumber(int number){
+        for (int i = 1; i < number + 1; i++) {
+            System.out.println(i);
+        }
+
+        
+    }   
+}
