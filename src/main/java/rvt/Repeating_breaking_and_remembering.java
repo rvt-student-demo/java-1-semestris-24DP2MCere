74adfcd128f7bec36db6fabda01457977d90ceb1
24DP2Mcere <A240480MC@rvt.lv>
2025-10-07 12:33:44 +0300

    Repeating,breaking and remembering

---
+++ src/main/java/rvt/Repeating_breaking_and_remembering.java
@@ -0,0 +1,34 @@
+package rvt;
+import java.util.Scanner;
+public class Repeating_breaking_and_remembering {
+
+public static void main(String[] args ){
+Scanner reader = new Scanner(System.in);
+
+System.out.println("Give a number: ");
+int number =  reader.nextInt();
+int summa = 0 ;
+int i = 0 ;
+int even = 0 ;
+int odd = 0 ;
+while( number != -1 ) {
+    summa += number ;
+    number = reader.nextInt();
+    i++ ;
+    if (number % 2 == 0 ) {
+        even++ ;
+    }
+    else{
+        odd++ ;
+    }
+}
+
+System.out.println("Thx!Bye!");
+System.out.println(summa);
+System.out.println("number: " + i);
+System.out.println("Average: " + (double)summa/i  );
+System.out.println("Even: "+ even + " Odd: "+ odd );
+
+
+}    
+}
