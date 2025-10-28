8c5c87ebbfa50a5e1620e6b3449c9c0dfa06addd
24DP2Mcere <A240480MC@rvt.lv>
2025-10-28 10:29:06 +0200

    Delikatese uzdevums

--- src/main/java/rvt/Delikatese.java
+++ src/main/java/rvt/Delikatese.java
@@ -5,26 +5,26 @@ import java.util.Scanner;
 public class Delikatese {
         public static void main(String[] args) {
         Scanner reader = new Scanner(System.in) ;
+
         System.out.print("Ievadiet preci: ");  
         String prece = reader.nextLine();
+
         System.out.print("Ievadi cenu: "); 
         double price = reader.nextDouble();
+
         System.out.print("Ekspress piegade (ja == 1 vai ne == 0): ");
         double Ekspress = reader.nextDouble() ;
+
         double piegade = 0 ;
 
-        if (price == 10 | price > 10) {
-            piegade = 0 ;
-            if (Ekspress == 1) {
-                piegade = 3 ;
-            }
-        }else{
-            piegade = 2 ;
-            if (Ekspress == 1) {
-                piegade += 3 ;
-            }
+        if (price < 10) {
+            piegade += 2 ;
+        }
+        if (Ekspress == 1) {
+            piegade += 3 ;
         }
         double summa = price + piegade ;
+
         System.out.println("Rekins : ");
         System.out.println("   " + prece + "   " + price);
         System.out.println("   Piegade   " + piegade);
