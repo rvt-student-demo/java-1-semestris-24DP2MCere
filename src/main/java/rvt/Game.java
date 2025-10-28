20fdaae91218db2903fb9eafa9e58490598e60d6
24DP2Mcere <A240480MC@rvt.lv>
2025-10-28 10:28:51 +0200

    Game uzdevums

--- src/main/java/rvt/Game.java
+++ src/main/java/rvt/Game.java
@@ -5,31 +5,26 @@ import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
-        
-    
-     Scanner reader = new Scanner(System.in);
-    Random random = new Random();
-
-    int result = random.nextInt(10) + 1;
-
-    System.out.println("Es domaju par skaitli no 1 lidz 10");
-    System.out.println("Ievadi minejumu: ");
-
-    int guestammount = 0;
-    while (guestammount < 3) {
-      int minejums = reader.nextInt();
-      if (minejums == result) {
-        System.out.println("Pareizi!!");
-        System.out.println("Tu esi uzvarej speli.");
-        break ;
-      } else {
-        System.out.println("nepareizi");
-      }
-      guestammount++;
-      if (guestammount == 3) {
-        System.out.println("Pareizais skaitli bija " + result);
-        System.out.println("Tu esi zaudejis speli");
-      }
+        Scanner reader = new Scanner(System.in);
+        Random random = new Random();
+        int result = random.nextInt(10) + 1;
+        System.out.println("Es domaju par skaitli no 1 lidz 10");
+        System.out.println("Ievadi minejumu: ");
+        int guestammount = 0;
+        while (guestammount < 3) {
+            int minejums = reader.nextInt();
+            if (minejums == result) {
+                System.out.println("Pareizi!!");
+                System.out.println("Tu esi uzvarej speli.");
+                break;
+            } else {
+                System.out.println("nepareizi");
+            }
+            guestammount++;
+            if (guestammount == 3) {
+                System.out.println("Pareizais skaitli bija " + result);
+                System.out.println("Tu esi zaudejis speli");
+            }
+        }
     }
- }
 }
